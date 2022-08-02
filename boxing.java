3df7b5e0e731f30a9c1e01b6c652988d61adb7e4
MeghaShinge28 <109073386+MeghaShinge28@users.noreply.github.com>
2022-08-02 14:28:09 +0530

    Add files via upload

---
+++ boxing.java
@@ -0,0 +1,10 @@
+class boxing
+{
+	public static void main(String args[])
+	{
+		int a = 40;
+		Integer b2 = new Integer(a);
+		Integer b3 = 4;
+		System.out.println(b2+" "+b3);
+	}
+}
---
+++ unboxing.java
@@ -0,0 +1,9 @@
+class unboxing
+{
+	public static void main(String args[])
+	{
+		Integer i = new Integer(30);
+		int a = i;
+		System.out.println(a);
+	}
+}
